5969a9e5693c0f36e583dd5cdfc90075b1fe4945
Peter Aigner <mastapetz@icloud.com>
2022-07-19 10:39:51 +0200

    import prework

---
+++ Peter/home.php
@@ -0,0 +1,115 @@
+<?php
+
+// INDEX ALT
+
+session_start();
+require_once 'components/db_connect.php';
+
+$sql = "SELECT * FROM dishes";
+$result = mysqli_query($connect ,$sql);
+$tbody=''; //this variable will hold the body for the table
+if(mysqli_num_rows($result)  > 0) {     
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){         
+        $tbody .= "<tr>
+            <td><img class='img-thumbnail' src='pictures/" .$row['image']."'</td>
+            <td>" .$row['name']."</td>
+            <td>" .$row['price']."</td>
+            <td><a href='d3_update.php?id=" .$row['dishID']."'><button class='btn btn-primary btn-sm' type='button'>Edit</button></a>
+            <a href='d3_delete.php?id=" .$row['dishID']."'><button class='btn btn-danger btn-sm' type='button'>Delete</button></a></td>
+            </tr>";
+    };
+} else {
+    $tbody =  "<tr><td colspan='5'><center>No Data Available </center></td></tr>";
+}
+
+
+// HOME
+
+
+
+// if adm will redirect to dashboard
+if (isset($_SESSION['adm'])) {
+    header("Location: dashboard.php");
+    exit;
+}
+// if session is not set this will redirect to login page
+if (!isset($_SESSION['adm']) && !isset($_SESSION['user'])) {
+    header("Location: index.php");
+    exit;
+}
+
+// select logged-in users details - procedural style
+$res = mysqli_query($connect, "SELECT * FROM users WHERE id=" . $_SESSION['user']);
+$row = mysqli_fetch_array($res, MYSQLI_ASSOC);
+
+mysqli_close($connect);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome - <?php echo $row['first_name']; ?></title>
+    <?php require_once 'components/boot.php' ?>
+    <style>
+        .userImage {
+            width: 200px;
+            height: 200px;
+        }
+
+        .hero {
+            background: rgb(2, 0, 36);
+            background: linear-gradient(24deg, rgba(2, 0, 36, 1) 0%, rgba(0, 212, 255, 1) 100%);
+        }
+        .manageProduct {           
+                margin: auto;
+            }
+            .img-thumbnail {
+                width: 70px !important;
+                height: 70px !important;
+            }
+            td {          
+                text-align: left;
+                vertical-align: middle;
+            }
+            tr {
+                text-align: center;
+            }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="hero">
+            <img class="userImage" src="pictures/<?php echo $row['picture']; ?>" alt="<?php echo $row['first_name']; ?>">
+            <p class="text-white">Hi <?php echo $row['first_name']; ?></p>
+        </div>
+        <a href="logout.php?logout">Sign Out</a>
+        <a href="update.php?id=<?php echo $_SESSION['user'] ?>">Update your profile</a>
+    </div>
+
+    <br><br><br>
+
+    <div class="manageProduct w-75 mt-3">    
+            <div class='mb-3'>
+                <a href= "create.php"><button class='btn btn-primary'type="button" >Add product</button></a>
+            </div>
+            <p class='h2'>Products</p>
+            <table class='table table-striped'>
+                <thead class='table-success'>
+                    <tr>
+                        <th>Picture</th>
+                        <th>Name</th>
+                        <th>price</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?= $tbody;?>
+                </tbody>
+            </table>
+        </div>
+</body>
+</html>
